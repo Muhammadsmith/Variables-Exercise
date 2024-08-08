830b511ee5c36d35125c8279b2cadb2e1fcd066e
muhammad <m.doug98@hotmail.com>
2024-08-08 12:06:48 -0400

    excercise complete

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Ron";
+            int number = 25;
+            char choice = 'a';
+            bool answer = true;
+            double price = 100.99;
+            decimal count = 10000000.9m;
+            Console.WriteLine($"Hi, my name is {name} i am {number} years old.");
+            Console.WriteLine($"{choice} {answer} statement about {name} is he spends {price} dollars a week on food.");
+            Console.WriteLine($"{name} also hopes to make {count} this year.");
         }
     }
 }
